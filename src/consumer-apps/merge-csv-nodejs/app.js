ec8e21210ef68f7d6987ab541425c96e82342f21
Sujit Chaurasia <trionfo@Sujits-MacBook-Air.local>
2021-01-05 16:34:59 +0530

    Renamed URLs to relevant ones

--- src/consumer-apps/merge-csv-nodejs/app.js
+++ src/consumer-apps/merge-csv-nodejs/app.js
@@ -29,7 +29,7 @@ app.get('/', function (req, res) {
 });
 
 // Receive Message
-app.post('/get_data', function (req, res) {
+app.post('/post_data', function (req, res) {
 
   try {
     var req_body = req.body;
